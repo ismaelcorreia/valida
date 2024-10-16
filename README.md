379af8103c5b4bbe98eca287f0f8dd8b28e9ba54
ismaelcorreia <ismaelmyname@hotmail.com>
2024-10-16 07:01:46 +0100

    Custom README

---
+++ README.md
@@ -0,0 +1,42 @@
+# Valida 
+<img src="logo.png" width="100" style="display: inline" alt="logo"/>
+
+___
+## Descrição
+
+Valida é uma webtool para validar certos comprovativos de pagamentos em PDF. Esta ferramenta foi desenvolvida com o objetivo de fornecer uma solução robusta e fácil de usar para a verificação da autenticidade de documentos PDF. A ferramenta inclui integração via API e uma interface de usuário amigável.
+
+___
+## Características
+
+- ✅ **Validação de PDFs**: Verifica a autenticidade de documentos PDF assinados digitalmente.
+- 🔗 **Integração via API**: Permite a integração com outros sistemas através de uma API.
+- 🌟 **Interface de Usuário Amigável**: UI user friendly para facilitar a interação dos usuários.
+
+___
+## Tecnologias Utilizadas
+
+- **Frontend**: Nuxt.js, PrimeVue
+- **Backend**: Java Spring Boot
+- **Bibliotecas**: Apache PDFBox, BouncyCastle, iText
+- **API**: RESTful API
+___
+## Instalação
+
+### Pré-requisitos
+
+- Java Development Kit (JDK) 11 ou superior
+- Maven
+- Node.js (para o frontend)
+
+### Passos de Instalação
+
+1. **Clonar o Repositório**
+
+   ```bash
+   git clone https://github.com/seu-usuario/valida.git
+   cd valida
+___
+> #### Desenvolvido com ❤️ em Angola por Ismael Correia.
+
+
--- src/test/java/com/ideafactory/valida/ReadingTest.java
+++ src/test/java/com/ideafactory/valida/ReadingTest.java
@@ -1,4 +1,57 @@
 package com.ideafactory.valida;
 
+import com.ideafactory.valida.utils.PdfVerifier;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 public class ReadingTest {
+
+    static final String testDocument = "teste.pdf";
+    static final String testFakerDocument = "teste-faker.pdf";
+    static final String testProof= "teste-proof.pdf";
+
+    static PdfVerifier pdfVerifier;
+    @BeforeAll
+    static void setup() {
+        pdfVerifier = new PdfVerifier();
+    }
+
+    @Test
+    void checkFile(){
+        Assertions.assertTrue(new File(testDocument).exists());
+    }
+
+    @Test
+    public void testIsPdfSigned() {
+        assertTrue(pdfVerifier.isPdfSigned(testFakerDocument));
+    }
+
+    @Test
+    public void testPrintSignatureDetails() {
+        pdfVerifier.printSignatureDetails(testFakerDocument);
+    }
+
+    @Test
+    public void testPrintDetails() {
+//        pdfVerifier.printPdfProperties(testProof);
+//        Produtor: iText® 7.0.5 ©2000-2017 iText Group NV (AGPL-version);
+//        modified using iText® 5.5.13.3 ©2000-2022 iText Group NV (AGPL-version)
+
+        pdfVerifier.printPdfProperties(testDocument);
+    }
+
+    @Test
+    public void testPrintDetails() {
+//        pdfVerifier.printPdfProperties(testProof);
+//        Produtor: iText® 7.0.5 ©2000-2017 iText Group NV (AGPL-version);
+//        modified using iText® 5.5.13.3 ©2000-2022 iText Group NV (AGPL-version)
+
+        pdfVerifier.printPdfProperties(testDocument);
+    }
 }
